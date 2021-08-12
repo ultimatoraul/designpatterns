2ce3e6a4994a9770529130389917162a14cf48e9
ultimatoraul <raulsp@ymail.com>
2021-08-12 12:15:10 -0300

    Criando novo projeto

---
+++ RSPires.DesingPatterns/RSPires.DesingPatterns.ObserverPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RSPires.DesingPatterns.ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
